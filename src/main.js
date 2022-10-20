efbe298254400c4768fbd7660746e1d7cd7baaf9
rafael costa <rafael.costa@icommgroup.com.br>
2022-10-20 14:14:03 -0300

    validando data de expiraçao e codigo de segurança

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -22,4 +23,38 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 // Para conseguirmos testar no Console >> globalThis.setCardType("nubank");
-globalThis.setCardType = setCardType
+globalThis.setCardType = setCardType;
+
+//Codigo de segurança (securityCode)
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+
+// aqui pegamos a const 
+// (securityCode) que vem do html pelo ID + 
+// (securityCodePattern) que tem a mascara de 4 digitos
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+//Data de expiração do cartao
+
+//(YY) dentro do blocs >> usei o date para pegar o dia completo e corto em 2 com .slice dentro do from e no (to) pego o ANO atual + 10 dando o valor limite também
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
